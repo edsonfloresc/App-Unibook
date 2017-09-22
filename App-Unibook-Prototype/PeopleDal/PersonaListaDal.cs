fdd951aa896cad5d09b0e499e6aa60bf1b073413
Your Name <antonio10mamani@gmail.com>
2017-09-22 13:45:48 -0400

    PersonaLista agregada

---
+++ App-Unibook-Prototype/PeopleDal/PersonaListaDal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Univalle.Fie.Sistemas.UniBook.PeopleDal
+{
+    public class PersonaListaDal
+    {
+        public int Persona
+        { 
+            get;
+            set; 
+        }
+        public string Nombre
+        {
+            get;
+            set;
+        }
+    }
+}
