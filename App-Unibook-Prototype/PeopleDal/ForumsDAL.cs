53acc7b3033963df303ff21f8eceaad1da9a18af
Your Name <gonzalo.calle.clavel@gmail.com>
2017-09-22 13:32:45 -0400

    Added some logic for ForumsDal

---
+++ App-Unibook-Prototype/PeopleDal/ForumsDAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Univalle.Fie.Sistemas.UniBook.PeopleDal
+{
+    public class ForumsDAL
+    {
+        public int MyProperty { get; set; }
+        //public int MyProperty { get; set; }
+    }
+}
