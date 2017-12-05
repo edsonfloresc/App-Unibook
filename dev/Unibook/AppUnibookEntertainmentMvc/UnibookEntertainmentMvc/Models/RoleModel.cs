7806b0c05304512f357f941994d5616309431a89
Andres Jaldin <andresjaldin2010@gmail.com>
2017-12-05 12:15:13 -0400

    adding mvc

---
+++ dev/Unibook/AppUnibookEntertainmentMvc/UnibookEntertainmentMvc/Models/RoleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Univalle.Fie.Sistemas.UniBook.UnibookEntertainmentMvc.Models
+{
+    public class RoleModel
+    {
+        public short RoleId { get; set; }
+
+        [Required(ErrorMessage = "RequiredName")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
---
+++ dev/Unibook/AppUnibookEntertainmentMvc/UnibookEntertainmentMvc/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+
+namespace Univalle.Fie.Sistemas.UniBook.UnibookEntertainmentMvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("es-ES"),
+                    new CultureInfo("en-US")
+
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(culture: "es-ES", uiCulture: "es-ES");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
+            services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+            services.AddSingleton<IConfiguration>(Configuration);
+        
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddFile("Logs/appPrototype-{Date}.txt");
+
+            var localizationOption = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(localizationOption.Value);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Category}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
